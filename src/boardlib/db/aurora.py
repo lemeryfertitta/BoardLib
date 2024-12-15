333c62acc70d8da024c640fed6c45eef160c6f11
Marco de Jongh <1107647+marcodejongh@users.noreply.github.com>
2024-12-15 18:26:59 +1100

    Debugging SSL issues

--- src/boardlib/db/aurora.py
+++ src/boardlib/db/aurora.py
@@ -33,6 +33,7 @@ def download_database(board, output_file):
         headers={
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
         },
+        verify=False,
     )
     response.raise_for_status()
     outer_apk = io.BytesIO(response.content)
